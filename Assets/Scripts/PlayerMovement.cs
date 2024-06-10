26367c61c40cbf678bf6adfcc11112e4bc7e6835
JayG1212 <jaybgunderson@gmail.com>
2024-06-09 20:30:21 -0500

    Update PlayerMovement.cs
    I added running to the game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Declares the variable that will represent the speed of the player
     public float speed = 5f;
 
+    // Declares the variable taht will represent the running speed of the player
+    public float runSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,14 +31,29 @@ public class NewBehaviourScript : MonoBehaviour
         // Gets the input from the W and S keys, and the up and down arrows
         float moveVertical = Input.GetAxis("Vertical");
 
+        // Check if the left shift key (Which represents run) is being pressed 
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        float currentSpeed;
+
+
+        // Checks if isRunning is true, and if so will set current speed to runspeed
+        if (isRunning)
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
 
         // Creates a movement vector based on input, speed and frame time.
         // "new Vector3(moveHorizontal,0.0f, moveVertical)" represents the direction the player wants to move
-        //  "* speed"  will multiply the direction vector to control how fast the player moves
+        //  "* currrentSpeed" will multiply the direction vector, by either run speed or just speed, to control how fast the player mov
         // "* Time.deltaTime" ensures that the movement is the same across different frame rates, making the movement independent of it
         // So the movment variable will be the directional vector multiplied by the speed variable and tthen multiplied by the deltaTime
         // movement is a Vector that represents the direction and magnitude by which we want to move the player. 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * currentSpeed * Time.deltaTime;
 
 
         // "transform.Translate(movement)"  moves the player by the movement vector ensuring consistent player movement
